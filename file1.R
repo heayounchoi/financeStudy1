8a2ce1c08283af17b2a6d6725dd70c76734a455c
heayounchoi <heayounchoi@gmail.com>
2023-02-24 00:17:18 +0900

    한국거래소 산업별 현황 크롤링

--- file1.R
+++ file1.R
@@ -162,3 +162,55 @@ for (i in 0:1) {
 
 data = do.call(rbind, data)
 
+# 한국거래소 산업별 현황 크롤링
+library(httr)
+library(rvest)
+library(readr)
+
+gen_otp_url =
+  'http://data.krx.co.kr/comm/fileDn/GenerateOTP/generate.cmd'
+
+gen_otp_data = list(
+  mktId = 'STK',
+  trdDd = '20230223',
+  money = '1',
+  csvxls_isNo = 'false',
+  name = 'fileDown',
+  url = 'dbms/MDC/STAT/standard/MDCSTAT03901'
+)
+
+otp = POST(gen_otp_url, query = gen_otp_data) %>%
+  read_html() %>%
+  html_text()
+
+down_url = 'http://data.krx.co.kr/comm/fileDn/download_csv/download.cmd'
+
+down_sector_KS = POST(down_url, query = list(code = otp),
+                      add_headers(referer = gen_otp_url)) %>%
+  read_html(encoding = 'EUC-KR') %>%
+  html_text() %>%
+  read_csv()
+
+gen_otp_data = list(
+  mktId = 'KSQ', 
+  trdDd = '20230223',
+  money = '1',
+  csvxls_isNo = 'false',
+  name = 'fileDown',
+  url = 'dbms/MDC/STAT/standard/MDCSTAT03901'
+)
+
+otp = POST(gen_otp_url, query = gen_otp_data) %>%
+  read_html() %>%
+  html_text()
+
+down_sector_KQ = POST(down_url, query = list(code = otp),
+                      add_headers(referer = gen_otp_url)) %>%
+  read_html(encoding = 'EUC-KR') %>%
+  html_text() %>%
+  read_csv()
+
+down_sector = rbind(down_sector_KS, down_sector_KQ)
+
+ifelse(dir.exists('data'), FALSE, dir.create('data'))
+write.csv(down_sector, 'data/krx_sector.csv')
